e341ebc43c3754b202cdd2d651e2bfd93f44fc3c
rajsistec123-wq <rajsistec123@gmail.com>
2025-11-04 23:38:41 +0530

    Add files via upload

---
+++ # hello_git.py
@@ -0,0 +1,8 @@
+# hello_git.py
+# This is a simple Python script for GitHub demo
+
+def greet(name):
+    print(f"Hello, {name}! Welcome to Git and GitHub demo.")
+
+if __name__ == "__main__":
+    greet("Raj Shrivastava")
